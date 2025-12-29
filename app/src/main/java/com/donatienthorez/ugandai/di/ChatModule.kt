12b0829ecb661cec6958f2b586143e19bfd85b86
yirga reminick <yirgareminick@gmail.com>
2025-12-29 14:11:05 -0600

    Adjust chat DI module

--- app/src/main/java/com/donatienthorez/ugandai/di/ChatModule.kt
+++ app/src/main/java/com/donatienthorez/ugandai/di/ChatModule.kt
@@ -6,6 +6,8 @@ import com.donatienthorez.ugandai.chat.domain.usecase.ObserveMessagesUseCase
 import com.ugandai.ugandai.chat.domain.usecase.ResendMessageUseCase
 import com.ugandai.ugandai.chat.domain.usecase.SendChatRequestUseCase
 import com.ugandai.ugandai.chat.ui.ChatViewModel
+import com.ugandai.ugandai.logbook.data.LogBookRepository
+import com.ugandai.ugandai.logbook.ui.LogBookViewModel
 import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
 import org.koin.androidx.viewmodel.dsl.viewModel
@@ -23,4 +25,8 @@ val chatModule = module {
     single { SendChatRequestUseCase(openAIRepository = get(), conversationRepository = get()) }
     single { ResendMessageUseCase(openAIRepository = get(), conversationRepository = get()) }
     single { ObserveMessagesUseCase(conversationRepository = get()) }
+
+    // LogBook
+    single { LogBookRepository(androidContext()) }
+    viewModel { LogBookViewModel(get(), androidContext()) }
 }
